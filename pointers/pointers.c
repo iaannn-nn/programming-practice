b9b5d3abaa4c97b037d4756c51bd893f6ce3249c
yen <hoangyenmailbox@gmail.com>
2020-03-24 16:24:03 +0700

    pointers

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 
 void pointerExample(){
+	printf("pointerExample ...\n");
+	
 	int radius = 25;
 	int *radiusPtr = &radius;
 	int *otherRadiusPtr;
@@ -12,9 +14,38 @@ void pointerExample(){
 	printf("%p \n",radiusPtr);
 	printf("%p \n",&radiusPtr);
 	printf("%p \n", &otherRadiusPtr);
+
+	printf("\n");
+}
+
+void arrayStringExample(){
+	printf("Arrays and Strings are Pointers ...\n");
+
+	int arr[4] = {1,2,3,4};
+	int* intptr = arr;
+	for(int i =0; i<4; i++){
+		printf("%d \n", *intptr);
+		intptr++;
+	}
+
+	char chararray[4] = { 'a','b','c','d'};
+	char* str = "Hello World";
+	printf("%s \n",str);
+	printf("%c \n", *(str + 4));
+
+	printf("\n");
 }
 
 int main(){
-	pointerExample();
+	//pointerExample();
+	//arrayStringExample();
+	int length = 10;
+	int breadth = 5;
 
+	int *length_ptr = &length;
+	int *breadth_ptr = &breadth;
+
+	printf("Printing derefrenced pointers : \n");
+	printf("%d\n", *length_ptr);
+	printf("%d\n", *breadth_ptr);
 }
