9c3279e7dcfb19729ad00575aab261fc1243a6ec
yen <hoangyenmailbox@gmail.com>
2020-03-26 16:20:31 +0700

    linkedList

--- linkedList/linkedList.c
+++ linkedList/linkedList.c
@@ -6,25 +6,77 @@ struct node{
 	struct node* next;
 };
 
-// int Length(struct node* head){
-// 	struct node* current = head;
-// 	int count = 0 ;
-// 	while(current != NULL){
-// 		count++;
-// 		current = current->next;
-// 	}
-// 	return count;
-// 	//for (current = head; current!=NULL; current = current->next)
-// }
+int Length(struct node* head){
+	struct node* current = head;
+	int count = 0 ;
+	while(current != NULL){
+		count++;
+		current = current->next;
+	}
+	return count;
+}
 
-// struct node* BuildOneTwoThree(){
-// 	return NULL;
-// }
+int Length2(struct node* head){
+	struct node* current = head;
+	int count = 0;
+	for (current = head; current!=NULL; current = current->next){
+		count++;
+	}
+	return count;
+}
 
-// void Push(struct node** headRef, int newData){
+int Length3(struct node* head){
+	int count = 0;
+	while(head != NULL){
+		count++;
+		head = head->next;
+	}
+	return count;
+}
+struct node* BuildOneTwoThree(){
+	struct node* head = (struct node*) malloc(sizeof(struct node));
+	head->data = 1;
+	head->next = (struct node*) malloc(sizeof(struct node));
+	head->next->data =2;
+	head->next->next =(struct node*) malloc(sizeof(struct node));
+	head->next->next->data = 3;
+	head->next->next->next = NULL;
 
-// }
+	return head;
+}
 
+void LinkTest(){
+	struct node* head = BuildOneTwoThree();
+	struct node* newNode = NULL;
+
+	newNode = malloc(sizeof(struct node);
+	newNode->data = 1;
+	newNode->next = head;
+	head = newNode;
+}
+
+void Push(struct node** headRef, int newData){
+	struct node* newNode = malloc(sizeof(struct node));
+
+	newNode->data = data;
+	newNode->next = *headRef;
+	*headRef = newNode;
+}
+
+void PushTest(){
+	struct node* head = BuildOneTwoThree();
+
+	Push(&head, 1);
+	Push(&head, 13);
+}
+
+void PushTest2(){
+	struct node* head = NULL;
+
+	Push(&head, 1);
+	Push(&head, 2);
+	Push(&head, 3);
+}
 // void BasicsCaller(){
 // 	struct node* head;
 // 	int len;
@@ -54,25 +106,48 @@ void ChangeCaller(){
 	ChangeToNull(&head2);
 }
 
+struct node* addAtHead(){
+	struct node* head = NULL;
+	int i;
 
-void addAtHead(struct node** headRef, int data){
-	struct node* head = (struct node*)malloc(sizeof(struct node));
-	head->data = data;
-	head->next = *headRef;
-	*headRef = head;	
+	for(i=1; i< 6; i++){
+		Push(&head,i);
+	}
+
+	return(head);
 }
 
-void addAtHeadExample(){
+struct node* BuildWithSpecialCase(){
 	struct node* head = NULL;
-	head = (struct node*)malloc(sizeof(struct node));
-	head->data = 0;
-	head->next = NULL;
+	struct node * tail;
 
-	addAtHead(&head,10);
-	printf("%d",head->data);
+	int i;
+	Push(&head, 1);
+	tail = head;
+
+	for(i = 2; i< 6; i++){
+		Push(&(tail->next),i);
+		tail = tail->next;
+	}
+
+	return(head);
+}
+
+struct node* BuildWithDummyNode(){
+	struct node dummy;
+	struct node* tail = &dummy;
+
+	int i;
+
+	dummy.next = NULL;
+
+	for(i = 1; i<6; i++){
+		Push(&(tail->next),i);
+		tail = tail->next;
+	}
+	return (dummy.next);
 }
 
 int main(){
-	addAtHeadExample;
-	return 0;
+
 }
